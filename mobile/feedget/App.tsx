1b027897d562b9abd298063029cd43196d024bbb
lauraagra <contatolauraagra@gmail.com>
2022-06-09 19:32:06 +0000

    frontend and backend integration

--- mobile/feedget/App.tsx
+++ mobile/feedget/App.tsx
@@ -1,20 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { theme } from './src/theme';
+import { Widget } from '.src/components'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View style={{
+      flex: 1,
+      backgroundColor: theme.colors.background
+    }}>
+
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 });
