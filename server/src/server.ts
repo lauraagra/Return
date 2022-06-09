2aafaaf07278d90fce6343cf33cbb8ef4040f947
lauraagra <contatolauraagra@gmail.com>
2022-06-09 20:01:10 +0000

    bugs fix

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,6 @@ app.use(cors());
 app.use(express.json()); // ver se tem um corpo em formato json na requisição
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log("Up and Running");
 });
